2d3e2e22e17fb92a0b7ae2ee70f1794730cd3792
Wiras Adi <capsic.io@gmail.com>
2022-07-17 20:37:18 +0700

    CONFMANHOME env. variable minor fix

--- main.go
+++ main.go
@@ -7,12 +7,20 @@ const DEFAULTHOME = "/opt/capsic/confman"
 func main() {
 	// Set CONFMANHOME
 	defaultHome := true
+
+	// Get from env
+	if os.Getenv("CONFMANHOME") != "" {
+		defaultHome = false
+	}
+
+	// Get from argument
 	if len(os.Args[1:]) > 0 {
 		if os.Args[1] != "" {
 			os.Setenv("CONFMANHOME", os.Args[1])
 			defaultHome = false
 		}
 	}
+
 	if defaultHome {
 		os.Setenv("CONFMANHOME", DEFAULTHOME)
 	}
